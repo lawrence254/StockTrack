0f9be068ff88b9c43c442a8da3e8fb0bcc2777e5
Lawrence <lawrenslava1@gmail.com>
2018-06-11 06:47:54 +0300

    Bind the views using butterknife

--- app/src/main/java/com/lawrence254/stocktrack/Login.java
+++ app/src/main/java/com/lawrence254/stocktrack/Login.java
@@ -2,12 +2,29 @@ package com.lawrence254.stocktrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.username)EditText mEmail;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.btnlogin)Button mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLogin.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if(v == mLogin){
+
+        }
     }
 }
