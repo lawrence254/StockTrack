0f16bb499b1308a4f0038c7783d9452166dd7bfe
Lawrence <lawrenslava1@gmail.com>
2018-06-11 08:29:15 +0300

    Add methods to insert and retrieve data from sqlite database

--- app/src/main/java/com/lawrence254/stocktrack/Login.java
+++ app/src/main/java/com/lawrence254/stocktrack/Login.java
@@ -1,10 +1,15 @@
 package com.lawrence254.stocktrack;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,18 +18,43 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.username)EditText mEmail;
     @BindView(R.id.password) EditText mPassword;
     @BindView(R.id.btnlogin)Button mLogin;
+
+    SQLiteDatabase db;
+    SQLiteOpenHelper openHelper;
+    Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        openHelper=new DBHelper(this);
+        db = openHelper.getReadableDatabase();
+
         mLogin.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
         if(v == mLogin){
+            String pass = mPassword.toString();
+            String email = mEmail.toString();
 
+            cursor = db.rawQuery("SELECT * FROM " + DBHelper.TABLE_NAME + " WHERE " + DBHelper.uemail + "=? AND " + DBHelper.upass + "=?", new String[]{email, pass});
+            if (cursor != null) {
+                if (cursor.getCount() > 0) {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this,MainActivity.class);
+                    startActivity(intent);
+
+                } else if(cursor.getCount() < 1){
+                    Toast.makeText(getApplicationContext(), "Account doesn't exist", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Login error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
         }
     }
-}
+
--- app/src/main/java/com/lawrence254/stocktrack/Register.java
+++ app/src/main/java/com/lawrence254/stocktrack/Register.java
@@ -1,10 +1,16 @@
 package com.lawrence254.stocktrack;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,19 +23,49 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     @BindView(R.id.upassrepeat) EditText mPassRepeat;
     @BindView(R.id.register) Button mRegister;
 
+    SQLiteOpenHelper openHelper;
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
-
+        openHelper = new DBHelper(this);
         mRegister.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
-        if(v == mRegister){
+        if(v == mRegister) {
 
+            db = openHelper.getWritableDatabase();
+            String fname = mFirstName.getText().toString();
+            String lname = mLastName.getText().toString();
+            String pass = mPassword.getText().toString();
+            String passrepeat = mPassRepeat.getText().toString();
+            String email = mEmail.getText().toString();
+//            String phone=_txtphone.getText().toString();
+            if (fname != "" || lname != "" || pass != "" || passrepeat != "" && pass.equals(passrepeat)) {
+                insertdata(fname, lname, pass, email);
+                Toast.makeText(getApplicationContext(), "Registration successful. You can now login", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Register.this,Landing.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Please ensure that all fields are field", Toast.LENGTH_LONG).show();
+            }
+            }
         }
-    }
 
+    public void insertdata(String fname, String lname, String pass, String email){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBHelper.ufname, fname);
+        contentValues.put(DBHelper.ulname, lname);
+        contentValues.put(DBHelper.upass, pass);
+        contentValues.put(DBHelper.uemail, email);
+        long u_id = db.insert(DBHelper.TABLE_NAME, null, contentValues);
+        Log.d("Registration ", ": "+u_id);
+    }
 }
+
+
