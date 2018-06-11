788be8a277c1769e7459dfdb7f7b3da55574db19
Lawrence <lawrenslava1@gmail.com>
2018-06-11 06:48:17 +0300

    Add butterknife view binding

--- app/src/main/java/com/lawrence254/stocktrack/Register.java
+++ app/src/main/java/com/lawrence254/stocktrack/Register.java
@@ -2,12 +2,34 @@ package com.lawrence254.stocktrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.fname) EditText mFirstName;
+    @BindView(R.id.lname) EditText mLastName;
+    @BindView(R.id.uemail) EditText mEmail;
+    @BindView(R.id.upass) EditText mPassword;
+    @BindView(R.id.upassrepeat) EditText mPassRepeat;
+    @BindView(R.id.register) Button mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+        mRegister.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v){
+        if(v == mRegister){
+
+        }
+    }
+
 }
