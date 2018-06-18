2504360585fd767c52744d7223c0284eb47b7753
Lawrence <lawrenslava1@gmail.com>
2018-06-18 15:23:39 +0300

    Get the first character of a company name and center it in an image view

--- app/src/main/java/com/lawrence254/stocktrack/adapters/QuotesListAdapter.java
+++ app/src/main/java/com/lawrence254/stocktrack/adapters/QuotesListAdapter.java
@@ -1,12 +1,16 @@
 package com.lawrence254.stocktrack.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.lawrence254.stocktrack.R;
 import com.lawrence254.stocktrack.model.Quote;
 import com.lawrence254.stocktrack.model.StocksModel;
@@ -45,6 +49,7 @@ public class QuotesListAdapter extends RecyclerView.Adapter<QuotesListAdapter.Qu
         @BindView(R.id.sHigh)TextView mHigh;
         @BindView(R.id.sLow) TextView mLow;
         @BindView(R.id.sChange)TextView mChange;
+        @BindView(R.id.stockLetter)ImageView mLetter;
         private Context mContext;
 
         public QuotesViewHolder(View itemView){
@@ -53,6 +58,16 @@ public class QuotesListAdapter extends RecyclerView.Adapter<QuotesListAdapter.Qu
             mContext = itemView.getContext();
         }
         public void bindQuotes(StocksModel stocksModel){
+            char fchar = stocksModel.getQuote().getCompanyName().charAt(0);
+            String first = Character.toString(fchar);
+
+            ColorGenerator colorGenerator = ColorGenerator.MATERIAL;
+            int color = colorGenerator.getRandomColor();
+
+            TextDrawable drawable = TextDrawable.builder()
+                    .buildRound(first, color);
+            mLetter.setImageDrawable(drawable);
+
             mstockName.setText(stocksModel.getQuote().getCompanyName());
             mHigh.setText(String.valueOf(stocksModel.getQuote().getHigh()));
             mLow.setText(String.valueOf(stocksModel.getQuote().getLow()));
