556fc9dcc1a43344e99a8d5cb140b2463f10889d
Lawrence <lawrenslava1@gmail.com>
2018-06-20 12:08:14 +0300

    Load content into the home fragment

--- app/src/main/java/com/lawrence254/stocktrack/fragments/HomeFragment.java
+++ app/src/main/java/com/lawrence254/stocktrack/fragments/HomeFragment.java
@@ -3,29 +3,84 @@ package com.lawrence254.stocktrack.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.lawrence254.stocktrack.R;
+import com.lawrence254.stocktrack.adapters.QuotesListAdapter;
+import com.lawrence254.stocktrack.model.StocksModel;
+import com.lawrence254.stocktrack.service.IEXService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+        public ArrayList<StocksModel> mStocks = new ArrayList<>();
+    private QuotesListAdapter quotesListAdapter;
+    @BindView(R.id.recycler)RecyclerView mRecycler;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this,root);
+
+        getQuotes();
+        return root;
     }
+
+        private void getQuotes() {
+        final IEXService iexService = new IEXService();
+
+        iexService.loadStocks(new Callback(){
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                mStocks = iexService.processQuotes(response);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        quotesListAdapter = new QuotesListAdapter(getContext(),mStocks);
+                        mRecycler.setAdapter(quotesListAdapter);
+
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                        mRecycler.setLayoutManager(layoutManager);
+                        mRecycler.setHasFixedSize(true);
+                    }
+                });
+
+            }
+        });
+}
 
 }
