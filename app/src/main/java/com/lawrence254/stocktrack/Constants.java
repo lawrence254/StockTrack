30cb9460f3c8bacd9dec00741ed508047b9b0bfd
Lawrence <lawrenslava1@gmail.com>
2018-06-20 11:45:11 +0300

    Enable bottom navigation and add fragments that switch based on the item selected in the bottom navigation bar

--- app/src/main/java/com/lawrence254/stocktrack/Constants.java
+++ app/src/main/java/com/lawrence254/stocktrack/Constants.java
@@ -3,7 +3,7 @@ package com.lawrence254.stocktrack;
 public class Constants {
     public static final String BASE_URL = "https://api.iextrading.com/1.0/stock/market/batch";
     public static final String SYMBOLS = "symbols";
-    public static final String SYMBOLS_KEY = "aapl,msft,tsla,goog";
+    public static final String SYMBOLS_KEY = "aapl,msft,tsla,goog,nflx,intc,orcl,amzn,abmd,bsx,bdx,amid,aan";
     public static final String TYPES = "types";
     public static final String TYPES_KEY = "quote,news,chart";
     public static final String RANGE = "range";
