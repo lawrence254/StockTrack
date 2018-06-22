7c4160f427bfcb7280bc744340e6f9831b3bbb76
Lawrence <lawrenslava1@gmail.com>
2018-06-22 07:09:31 +0300

    Add fragments and implement a bottom Navgitaion bar

--- app/src/main/java/com/lawrence254/stocktrack/ui/MainActivity.java
+++ app/src/main/java/com/lawrence254/stocktrack/ui/MainActivity.java
@@ -1,15 +1,69 @@
 package com.lawrence254.stocktrack.ui;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.lawrence254.stocktrack.R;
+import com.lawrence254.stocktrack.fragments.HomeFragment;
+import com.lawrence254.stocktrack.fragments.NewsFragment;
+import com.lawrence254.stocktrack.fragments.ProfileFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.frameContainer)
+    FrameLayout mFrameLayout;
+    @BindView(R.id.navigationView)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        getQuotes();
+        //Init Fragments
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new HomeFragment());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_news:
+                    fragment = new NewsFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+//        Load the fragments
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameContainer,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
