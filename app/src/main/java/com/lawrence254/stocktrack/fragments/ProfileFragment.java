2ef504ebc40a9ac186f4dc604efe167a18c4c07b
Lawrence <lawrenslava1@gmail.com>
2018-07-02 14:10:05 +0300

    Populate and style the profile page with data from firebase data

--- app/src/main/java/com/lawrence254/stocktrack/fragments/ProfileFragment.java
+++ app/src/main/java/com/lawrence254/stocktrack/fragments/ProfileFragment.java
@@ -6,14 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lawrence254.stocktrack.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    @BindView(R.id.name)TextView mname;
+    @BindView(R.id.email) TextView memail;
+    @BindView(R.id.provider) TextView prov;
+    @BindView(R.id.img)ImageView mImg;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -24,7 +38,29 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this,root);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String fname =user.getDisplayName();
+        List<String> providers = user.getProviders();
+        String email = user.getEmail();
+        String img = String.valueOf(user.getPhotoUrl());
+
+        StringBuilder builder = new StringBuilder();
+        for (String details : providers) {
+            builder.append(details + "");
+        }
+
+        prov.setText("Account: "+builder.toString());
+
+        mname.setText("Name: "+fname);
+        memail.setText("Email: "+email);
+
+        Picasso.get().load(img).into(mImg);
+
+        return root;
     }
 
 }
